fe4346bc74dc9552ed97c83688a5d7e32ec8ec5d
Michael Cordero <mcordero88@gmail.com>
2016-04-27 01:35:22 -0500

    Perfect_Square_Vector_v1.0.0

--- Perfect_Square_Vector/main.cpp
+++ Perfect_Square_Vector/main.cpp
@@ -5,11 +5,46 @@
 //  Created by Michael Cordero on 4/27/16.
 //  Copyright © 2016 com.cordero.michael. All rights reserved.
 //
-
 #include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<int> create_perfect_square_vector(int a, int b){
+    
+    int max = fmax(a, b);
+    int min = fmin(a, b);
+    vector<int> v;
+    int series = max-min+1;
+    
+    for(int i=0;i<series;i++){
+        if(fmod(sqrt(min +i),1) == 0){
+            v.push_back(min+i);
+        }
+    }
+    return v;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    string input1="";
+    string input2="";
+    
+    cout << "Please enter two non-negative integers: \n";
+    getline(cin, input1);
+    int first = stoi(input1);
+    getline(cin, input2);
+    int second = stoi(input2);
+    
+    std::vector<int> v = create_perfect_square_vector(first, second);
+    
+    cout << "The number of perfect squares between those two integers are: \n";
+    
+    cout << "" << v.size() << endl;
+    
+    cout << "The perfect squares are: " << endl;
+    
+    for(int i =0; i<v.size();i++){
+        cout << " " << v[i] << endl;
+    }
 }

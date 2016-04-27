0fbaa81fa504526a0c98cf06c4be0e8cdeead565
Michael Cordero <mcordero88@gmail.com>
2016-04-27 01:34:00 -0500

    Initial Commit

---
+++ Perfect_Square_Vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Perfect_Square_Vector
+//
+//  Created by Michael Cordero on 4/27/16.
+//  Copyright © 2016 com.cordero.michael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
